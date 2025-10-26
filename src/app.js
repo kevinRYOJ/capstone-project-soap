28347e171eee4ddfe1b3daedc0870d46799c767a
kevinRYOJ <kevinryo33@gmail.com>
2025-10-26 12:56:20 +0700

    kae mang eror iki ga

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const cors = require("cors");
-const sequelize = require("./config/db"); // koneksi Sequelize
+const sequelize = require("./config/db");
 const app = express();
 
 // ====== Middleware ======
@@ -14,10 +14,10 @@ sequelize
     .catch((err) => console.error("❌ Gagal koneksi database:", err.message));
 
 // ====== Routes ======
-const authRoutes = require("./routes/auth"); // untuk login admin & member
-const adminRoutes = require("./routes/adminRoutes"); // untuk CRUD admin
-const memberRoutes = require("./routes/memberRoutes"); // untuk CRUD admin
-const kinerjaRoutes = require("./routes/kinerjaMemberRoutes");
+const authRoutes = require("./routes/auth");
+const adminRoutes = require("./routes/adminRoutes");
+const memberRoutes = require("./routes/memberRoutes");
+const kinerjaMemberRoutes = require("./routes/kinerjaMemberRoutes");
 const cabuyRoutes = require("./routes/cabuyRoutes");
 const surveyRoutes = require("./routes/surveyRoutes");
 const proyekRoutes = require("./routes/proyekRoutes");
@@ -26,11 +26,10 @@ const crmRoutes = require("./routes/crmRoutes");
 const rumahRoutes = require("./routes/rumahRoutes");
 const propertiRoutes = require("./routes/propertiRoutes");
 
-
 app.use("/api/auth", authRoutes);
 app.use("/api/admin", adminRoutes);
 app.use("/api/member", memberRoutes);
-app.use("/api/kinerja-member", kinerjaRoutes);
+app.use("/api/kinerja-member", kinerjaMemberRoutes);
 app.use("/api/cabuy", cabuyRoutes);
 app.use("/api/survey", surveyRoutes);
 app.use("/api/proyek", proyekRoutes);
@@ -39,7 +38,6 @@ app.use("/api/crm", crmRoutes);
 app.use("/api/rumah", rumahRoutes);
 app.use("/api/properti", propertiRoutes);
 
-
 // ====== Root Endpoint ======
 app.get("/", (req, res) => {
     res.send("🚀 API CP SOAP berjalan dengan baik!");
@@ -47,4 +45,6 @@ app.get("/", (req, res) => {
 
 // ====== Jalankan Server ======
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`✅ Server berjalan di http://localhost:${PORT}`));
+app.listen(PORT, () =>
+    console.log(`✅ Server berjalan di http://localhost:${PORT}`)
+);
